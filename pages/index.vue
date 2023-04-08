dfabc19ec92464d57c00890e392f9004fa993965
Kevin <kfconeone@gmail.com>
2023-04-08 23:07:00 +0800

    rc4

--- pages/index.vue
+++ pages/index.vue
@@ -109,7 +109,7 @@ async function sendEmail() {
                                 <div class="flex items-center justify-between">
                                     <a href="#" class="-m-1.5 p-1.5">
                                         <span class="sr-only">Your Company</span>
-                                        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                                        <img class="h-8 w-auto" src="@/assets/logo.png" alt="" />
                                     </a>
                                     <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="isMenuOpen = !isMenuOpen">
                                         <span class="sr-only">Close menu</span>
