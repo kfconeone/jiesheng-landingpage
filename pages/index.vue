004a74ad89f7f79dde3e126e8ff1f6fcd458ffb6
Kevin <kfconeone@gmail.com>
2023-04-08 15:32:28 +0800

    rc3

--- pages/index.vue
+++ pages/index.vue
@@ -381,11 +381,14 @@ async function sendEmail() {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 absolute right-2 top-2" @click="isContactOpen = false">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
-
-            <img class="w-40 h-40 mx-auto" src="https://qr-official.line.me/gs/M_761lwoyv_GW.png" />
+            <a href="https://lin.ee/14N72sU">
+                <img class="w-40 h-40 mx-auto" src="https://qr-official.line.me/gs/M_761lwoyv_GW.png" />
+            </a>
 
             <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">捷勝設計</div>
+                <a href="https://lin.ee/14N72sU">
+                    <div class="font-bold text-xl mb-2 text-center">捷勝設計</div>
+                </a>
                 <p class="text-gray-700 text-base">加入官方號直接與我們聯繫！</p>
             </div>
         </div>
