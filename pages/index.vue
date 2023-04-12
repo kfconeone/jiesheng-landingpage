9336efe7c3374ba01bd2c91763a0ca92f51739bd
Kevin <kfconeone@gmail.com>
2023-04-13 00:24:17 +0800

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -367,7 +367,7 @@ async function sendEmail() {
                 </ul>
             </div>
         </div>
-        you
+
         <!-- Contact Section -->
         <div class="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8 overflow-x-hidden" id="contact-section">
             <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true"></div>
