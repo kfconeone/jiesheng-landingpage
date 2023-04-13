1c71ff7de6a956db733d933c7c880d14990d6a2a
Kevin <kfconeone@gmail.com>
2023-04-13 22:15:38 +0800

    updated

--- server/api/contact.post.ts
+++ server/api/contact.post.ts
@@ -1,6 +1,7 @@
 export default defineEventHandler(async (event) => {
     const body = await readBody(event);
 
+    console.log(body);
     const nodemailer = await import("nodemailer");
     let transporter = nodemailer.createTransport({
         service: "gmail",
@@ -13,9 +14,10 @@ export default defineEventHandler(async (event) => {
     // create a mail options object
     let mailOptions = {
         from: "jiesheng.studio@gmail.com", // replace with your actual Gmail account email address
-        to: "kfconeone@gmail.com, powerjj@gmail.com", // replace with the recipient email address
-        subject: "Test Email", // replace with the email subject
-        text: "Hello World!", // replace with the email body
+        to: " jiesheng.studio@gmail.com", // replace with the recipient email address
+        // to: "kfconeone@gmail.com, powerjj@gmail.com, jiesheng.studio@gmail.com", // replace with the recipient email address
+        subject: `捷勝設計的諮詢信箱收到${body.company} - ${body.name}的諮詢`, // replace with the email subject
+        text: `${body.message}, 電話${body.phone}, email${body.email}`, // replace with the email body
     };
 
     // send the email
