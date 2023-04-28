fb52f74699df12745d2291e1abf338956444ce72
kfconeone <kfconeone@gmail.com>
2023-04-28 10:55:43 +0800

    updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            script: [
+                { src: "https://www.googletagmanager.com/gtag/js?id=AW-623112803" },
+                {
+                    children: `
+                        console.log('Hello, world!');
+
+
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'AW-623112803');
+
+                        gtag('event', 'conversion', {'send_to': 'AW-623112803/sc6oCP7U65oYEOPkj6kC'});
+            
+            `,
+                },
+            ],
+        },
+    },
     css: ["~/assets/css/main.css"],
     postcss: {
         plugins: {
